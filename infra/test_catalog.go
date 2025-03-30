c85328d6e1b6b14890f876cd1317cdea5e77cb73
ionrock <eric@ionrock.org>
2025-03-30 16:29:24 -0500

    test: Add test catalog infrastructure file

---
+++ infra/test_catalog.go
@@ -0,0 +1,4 @@
+package infra
+
+
+# Create a test for the Catalog that loads the YAML found in hosts.yml in the root of the repository. Ensure that we are able to load the YAML and get the expected Host objects. AI!
