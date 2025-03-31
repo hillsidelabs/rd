761f7ef64758e0d69a7de1134ba3d33480e59d9b
ionrock <eric@ionrock.org>
2025-03-30 23:00:24 -0500

    Avoid adding hander to handler functions

--- web/infra.go
+++ web/infra.go
@@ -15,27 +15,6 @@ func (s *Server) Infra() http.Handler {
 			return
 		}
 
-		component := pages.Catalog(catalog)
-		component.Render(r.Context(), w)
-	})
-}
-package web
-
-import (
-	"net/http"
-
-	"github.com/hillsidelabs/rd/infra"
-	"github.com/hillsidelabs/rd/web/ui/pages"
-)
-
-func (s *Server) InfraHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		catalog, err := infra.NewCatalog()
-		if err != nil {
-			http.Error(w, "Failed to load infrastructure catalog: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-
 		component := pages.Infra(catalog)
 		component.Render(r.Context(), w)
 	})
